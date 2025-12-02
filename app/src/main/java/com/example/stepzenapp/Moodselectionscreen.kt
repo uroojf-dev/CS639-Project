8ea2be062825a4e7d3949a2795f7146d38d3cd4a
iabubakartahir <77561458+iabubakartahir@users.noreply.github.com>
2025-12-02 19:52:12 +0500

    Abubakar Tahir Update Moodselectionscreen.kt by adding code

--- app/src/main/java/com/example/stepzenapp/Moodselectionscreen.kt
+++ app/src/main/java/com/example/stepzenapp/Moodselectionscreen.kt
@@ -1,1 +1,19 @@
+//Push 4-- Abubakar-Add MoodRepository
 
+
+package com.example.stepzenapp.repository
+
+import com.example.stepzenapp.model.Mood
+
+class MoodRepository {
+
+    private val moodList = mutableListOf<Mood>()
+
+    fun addMood(mood: Mood) {
+        moodList.add(mood)
+    }
+
+    fun getAllMoods(): List<Mood> {
+        return moodList.toList()
+    }
+}
