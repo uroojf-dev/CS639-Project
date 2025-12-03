c5af3e558b6dda86e729ff1ca1711a3ff50b5a7e
uroojfatima <uroozk2@gmail.com>
2025-12-03 13:27:13 -0500

    Push 8 -Urooj-  Implemented basic navigation between Home and Mood screens

--- app/src/main/java/com/example/stepzenapp/MainActivity.kt
+++ app/src/main/java/com/example/stepzenapp/MainActivity.kt
@@ -3,16 +3,34 @@ package com.example.stepzenapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.stepzenapp.ui.theme.StepzenTheme
+import com.example.stepzenapp.viewmodel.MoodViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StepzenTheme {
-                HomeScreen()
+                val navController = rememberNavController()
+                val viewModel = MoodViewModel()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = "home"
+                ) {
+                    composable("home") {
+                        HomeScreen(
+                            onMoodClick = { navController.navigate("mood") }
+                        )
+                    }
+                    composable("mood") {
+                        MoodSelectionScreen(viewModel)
+                    }
+                }
             }
         }
     }
-
 }
