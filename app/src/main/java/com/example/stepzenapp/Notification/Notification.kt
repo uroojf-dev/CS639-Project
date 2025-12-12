2143a88221dd705764cb13bfdfa113205047837e
iabubakartahir <77561458+iabubakartahir@users.noreply.github.com>
2025-12-12 17:50:53 +0500

    Abubakar Tahir PUSH 29 - Notification.kt

---
+++ app/src/main/java/com/example/stepzenapp/Notification/Notification.kt
@@ -0,0 +1,27 @@
+// Abubakar Tahir PUSH 29 - Notification.kt
+
+package com.example.stepzen.Notification
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.example.HealthyMode.R
+
+const val NOTIFICATION_ID=4
+const val channel_ID="channel"
+const val titleExtra="titleExtra"
+const val messageExtra="messageExtra"
+class Notification:BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val notification=NotificationCompat.Builder(context, channel_ID)
+            .setSmallIcon(R.drawable.mainlogo)
+            .setContentTitle(intent.getStringExtra(titleExtra))
+            .setContentText(intent.getStringExtra(messageExtra))
+            .setPriority(NotificationCompat.PRIORITY_MAX).setStyle(NotificationCompat.BigTextStyle().bigText(intent.getStringExtra(messageExtra)))
+            .build()
+        val manager=context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(NOTIFICATION_ID,notification)
+    }
+}
