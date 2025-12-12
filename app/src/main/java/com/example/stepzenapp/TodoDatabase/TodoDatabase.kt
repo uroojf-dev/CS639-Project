38032eb3205aba653c3824cdfe135557b7ab6725
iabubakartahir <77561458+iabubakartahir@users.noreply.github.com>
2025-12-12 15:03:19 +0500

    Abubakar Tahir PUSH 23 - TodoDatabase.kt

---
+++ app/src/main/java/com/example/stepzenapp/TodoDatabase/TodoDatabase.kt
@@ -0,0 +1,11 @@
+// Abubakar Tahir PUSH 23 - TodoDatabase.kt
+
+package com.example.stepzen.TodoDatabase
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Todo::class], version = 3)
+abstract class TodoDatabase:RoomDatabase() {
+    abstract fun todoDao():TodoDao
+}
