58e1e840b4fa564433b2b3e5d498829c1b1e80de
iabubakartahir <77561458+iabubakartahir@users.noreply.github.com>
2025-12-12 15:07:07 +0500

    Abubakar Tahir PUSH 24 - TodoDao.kt

---
+++ app/src/main/java/com/example/stepzenapp/TodoDatabase/TodoDao.kt
@@ -0,0 +1,21 @@
+// Abubakar Tahir PUSH 24 - TodoDao.kt
+
+package com.example.stepzen.TodoDatabase
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface TodoDao {
+    @Query("SELECT * from Todo ORDER BY time ASC,status DESC")
+    fun getTodo():LiveData<List<Todo>>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTodo(todo: Todo)
+    @Delete
+    suspend fun deleteTodo(todo: Todo)
+    @Update
+    suspend fun updateTodo(todo: Todo)
+    @Query("SELECT * from Todo WHERE id= :id")
+    suspend fun getTodoById(id: Int): Todo?
+
+}
